71c04351933c80d2500a702bee8363016280db16
jeremyspiera <42815292+jeremyspiera@users.noreply.github.com>
2018-12-02 23:03:53 -0500

    Update PercolationDFSFast.java

--- src/PercolationDFSFast.java
+++ src/PercolationDFSFast.java
@@ -6,7 +6,10 @@ public class PercolationDFSFast extends PercolationDFS {
 	
 	@Override
 	protected void updateOnOpen(int row, int col) {
-		if (row == 0) dfs(row,col);
+		if (row == 0) {
+			dfs(row,col);
+			return;
+		}
 		if (inBounds(row-1,col) && isOpen(row-1,col) && isFull(row-1,col)) {
 			dfs(row,col);
 			return;
