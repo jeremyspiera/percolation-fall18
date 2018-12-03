2940b1dd958fd0225828bbccca159676a9c20b21
jeremyspiera <42815292+jeremyspiera@users.noreply.github.com>
2018-12-02 22:51:03 -0500

    Update PercolationBFS.java

--- src/PercolationBFS.java
+++ src/PercolationBFS.java
@@ -15,25 +15,21 @@ public class PercolationBFS extends PercolationDFSFast{
 		Queue<Integer> q = new LinkedList<>(); 
 		myGrid[row][col] = FULL;
 		q.add(row*myGrid.length+col);
+		int [] rr = {-1,0,1};
+		int [] cc = {-1,0,1};
 		while(q.size() > 0) {
 			Integer x = q.poll();
 			int r = x / myGrid.length;
 			int c = x % myGrid.length;
-			if (inBounds(r+1,c) && isOpen(r+1,c)&& !isFull(r+1,c)) {
-				myGrid[r+1][c] = FULL;
-				q.add((r+1) * myGrid.length + c);
-			}
-			if (inBounds(r-1,c) && isOpen(r-1,c) && !isFull(r-1,c)) {
-				myGrid[r-1][c] = FULL;
-				q.add((r-1) * myGrid.length + c);
-			}
-			if (inBounds(r,c+1) && isOpen(r,c+1) && !isFull(r,c+1)) {
-				myGrid[r][c+1] = FULL;
-				q.add(r * myGrid.length + c + 1);
-			}
-			if (inBounds(r,c-1) && isOpen(r,c-1) && !isFull(r,c-1)) {
-				myGrid[r][c-1] = FULL;
-				q.add(r * myGrid.length + c-1);
+			for (int i = 0; i < rr.length; i++) {
+				for (int j = 0; j < cc.length; j++) {
+					if (i == 0 || j == 0) {
+						if (inBounds(r+i,c+j) && isOpen(r+i,c+j)&& !isFull(r+i,c+j)) {
+							myGrid[r+i][c+j] = FULL;
+							q.add((r+i) * myGrid.length + c+j);
+						}
+					}
+				}
 			}
 		}
 	}
